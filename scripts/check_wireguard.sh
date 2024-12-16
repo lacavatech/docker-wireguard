8d58da0ba891266a618b580bda164c119e6b9f0a
alacava <me@anthonylacava.com>
2024-12-16 13:30:24 -0500

    Fixed messages again

--- scripts/check_wireguard.sh
+++ scripts/check_wireguard.sh
@@ -4,12 +4,12 @@ INTERFACE_CHECK=${INTERFACE}
 
 while true; do
    if ! wg show $INTERFACE_CHECK | grep "listening"; then
-        echo "WireGuard connection down. Restarting...${$INTERFACE_CHECK}"
+        echo "WireGuard connection down. Restarting...${INTERFACE_CHECK}"
         wg-quick down $INTERFACE_CHECK
         wg-quick up $INTERFACE_CHECK
    
    else
-   echo "WireGuard connection up....${$INTERFACE_CHECK}"
+   echo "WireGuard connection up....${INTERFACE_CHECK}"
    fi
 
      sleep 60

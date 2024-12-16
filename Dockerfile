3068492ec0997e88a56aa3d0090aa18b09f91d4f
alacava <me@anthonylacava.com>
2024-12-16 13:00:54 -0500

    Updated script to check the right interface

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM linuxserver/wireguard
 
+ENV PTUSERNAME=wg0
+
 COPY scripts/check_wireguard.sh /check_wireguard.sh
 RUN chmod +x /check_wireguard.sh
 
--- scripts/check_wireguard.sh
+++ scripts/check_wireguard.sh
@@ -1,10 +1,10 @@
    #!/bin/bash
 
    while true; do
-       if ! wg show wg0 | grep -q "latest handshake"; then
+       if ! wg show $interface | grep -q "latest handshake"; then
            echo "WireGuard connection down. Restarting..."
-           wg-quick down wg0
-           wg-quick up wg0
+           wg-quick down $interface
+           wg-quick up $interface
        fi
        sleep 60
    done
